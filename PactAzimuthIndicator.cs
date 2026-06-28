2951a57362eb152eec178a26575747a541e9e8cc
Bluehawk2008 <bluehawk2008@gmail.com>
2026-06-28 18:01:14 -0400

    Update PactAzimuthIndicator.cs
    Variable clock size made available, and option to mark targets on the clock instead of the overhead compass.

--- PactAzimuthIndicator.cs
+++ PactAzimuthIndicator.cs
@@ -1,5 +1,6 @@
-﻿using System.IO;
+using System.IO;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,7 +14,7 @@ using MelonLoader;
 using PactAzimuthIndicatorMod;
 using HarmonyLib;
 
-[assembly: MelonInfo(typeof(PactAzimuthIndicator), "Pact Azimuth Indicator", "1.0.1", "Bluehawk")]
+[assembly: MelonInfo(typeof(PactAzimuthIndicator), "Pact Azimuth Indicator", "1.1.0", "Bluehawk")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace PactAzimuthIndicatorMod
@@ -34,15 +35,14 @@ namespace PactAzimuthIndicatorMod
                 var _sbRef = AccessTools.FieldRefAccess<WeaponHud, StringBuilder>("_sb");
                 StringBuilder weapSB = _sbRef(_weaponHUD);
                 temp = weapSB.ToString();
-
                 
                 float turretHeading = _aimablePlatform.LocalRotation.eulerAngles.y;
                 int mils = (int)(turretHeading * 16.667f);
                 if (mils > 3000) mils -= 3000; else mils += 3000;                
-                string milsString = mils.ToString();                
-                if (mils > 100 && mils < 1000) milsString = milsString.Insert(0, "0");
-                if (mils == 100) milsString += "0"; 
-                if (mils < 100) milsString = milsString.Insert(0, "00");
+                string milsString = mils.ToString(); 
+                if (mils < 10) milsString = milsString.Insert(0, "000"); 
+                else if (mils < 100) milsString = milsString.Insert(0, "00");
+                else if (mils < 1000) milsString = milsString.Insert(0, "0");
                 milsString = milsString.Insert(2, "-");
                 _weaponText.text = temp + "\n" + milsString + " mils";                
             }
@@ -56,24 +56,57 @@ namespace PactAzimuthIndicatorMod
         }
 
     }
+
+    public class ClockTargetManager : MonoBehaviour
+    {
+        public Compass _compass;
+        public GameObject _targetMarker;
+        public PlayerInput playerInput;
+        public NwhChassis _vehicle;
+
+        public void LateUpdate()
+        {
+            var compPairRef = AccessTools.FieldRefAccess<Compass, List<Compass.CompassPointImagePair>>("_compassPairs");
+            List<Compass.CompassPointImagePair> compassPairs = compPairRef(_compass);            
+            if (compassPairs.Count == 0) {
+                _targetMarker.SetActive(false);
+                return;
+            }
+            
+            _targetMarker.SetActive(true);
+            compassPairs[0].Image.color = new Color(0f, 0f, 0f, 0f);
+            _vehicle = playerInput.CurrentPlayerChassis as NwhChassis;
+            Vector3 targetPosition = compassPairs[0].CompassPoint.Position;
+            float angle = Vector2.SignedAngle(new Vector2(targetPosition.x, targetPosition.z) - new Vector2(_vehicle.transform.position.x, _vehicle.transform.position.z), 
+                new Vector2(_vehicle.transform.forward.x, _vehicle.transform.forward.z));            
+            
+            _targetMarker.transform.GetComponent<RectTransform>().localEulerAngles = new Vector3(0f, 0f, -angle);
+        }
+    }
     public class PactAzimuthIndicator : MelonMod
     {
         public static GameObject gameManager;
         public static Text weaponText;
 
         public static MelonPreferences_Entry<bool> clock_sprite;
+        public static MelonPreferences_Entry<bool> larger_clock;
         public static MelonPreferences_Entry<bool> mils_readout;
         public static MelonPreferences_Entry<bool> pact_only;
+        public static MelonPreferences_Entry<bool> clock_targets;
 
         public override void OnInitializeMelon()
         {
             MelonPreferences_Category cfg = MelonPreferences.CreateCategory("Pact Azimuth Indicator");
             clock_sprite = cfg.CreateEntry<bool>("Modify the hull sprite", true);
             clock_sprite.Comment = "Replaces default hull-turret diagram with a clock face in 500 mil intervals";
+            larger_clock = cfg.CreateEntry<bool>("Larger hull-turret diagram", false);
+            larger_clock.Comment = "Increases the size of the diagram for readability";
             mils_readout = cfg.CreateEntry<bool>("Adds milliradian readout to HUD", true);
             mils_readout.Comment = "Adds the precise mil heading of the turret to the HUD, in Soviet format (30-00 is straight ahead)";
             pact_only = cfg.CreateEntry<bool>("Pact vehicles only", false);
             pact_only.Comment = "Modifies the HUD only when playing Warsaw Pact vehicles";
+            clock_targets = cfg.CreateEntry<bool>("Mark targets on azimuth indicator", true);
+            clock_targets.Comment = "Designated targets will appear on the azimuth indicator instead of on the compass";
         }
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
@@ -86,7 +119,7 @@ namespace PactAzimuthIndicatorMod
         }
         public IEnumerator TurretClock(GameState _)
         {
-            Vehicle playerVic = GameObject.FindObjectOfType<PlayerInput>().CurrentPlayerDamageStatus as Vehicle;
+            Vehicle playerVic = Object.FindObjectOfType<PlayerInput>().CurrentPlayerDamageStatus as Vehicle;
             string playerVicShort;            
             if (playerVic.UniqueName.Length < 3) playerVicShort = playerVic.UniqueName + "    "; 
             else playerVicShort = playerVic.UniqueName.Substring(0, 3);
@@ -120,18 +153,45 @@ namespace PactAzimuthIndicatorMod
                 byte[] newClock_data = File.ReadAllBytes("Mods/PactAzimuthIndicator/azimuth_hull.png");
                 newClock.LoadImage(newClock_data);
                 Sprite newSprite = Sprite.Create(newClock, new Rect(0f, 0f, newClock.width, newClock.height), new Vector2(0.5f, 0.5f), 100f);
-                GameObject hull_go = gameManager.transform.Find("UIHUDCanvas/weapons text/azimuth HUD/hull").gameObject;
-                Image hull_img = hull_go.GetComponent<Image>();
-                hull_img.sprite = newSprite;
+                GameObject azimuthHUD = gameManager.transform.Find("UIHUDCanvas/weapons text/azimuth HUD").gameObject;                
+                Image hull_img = azimuthHUD.transform.Find("hull").GetComponent<Image>();
+                hull_img.sprite = newSprite;                
+                RectTransform turretRect = azimuthHUD.transform.Find("turret").GetComponent<RectTransform>();                
+                turretRect.localScale = new Vector3(0.6f, 1f, 1f);
+
+                if (clock_targets.Value)
+                {
+                    if (azimuthHUD.transform.Find("hull/target") == null) { 
+                        GameObject target = new GameObject("target");
+                        target.transform.parent = azimuthHUD.transform.Find("hull");
+                        RectTransform target_rect = target.AddComponent<RectTransform>();
+                        target_rect.anchoredPosition = new Vector2(0f, 0f);                        
+                        target_rect.localScale = new Vector3(0.6f, 1f, 1f);
+                        Image target_img = target.AddComponent<Image>();
+                        target_img.sprite = azimuthHUD.transform.Find("turret").GetComponent<Image>().sprite;
+                        target_img.color = new Color(1f, 0.2736f, 0f, 1f);
+
+                        ClockTargetManager clockTargetManager = azimuthHUD.AddComponent<ClockTargetManager>();
+                        clockTargetManager._targetMarker = target;
+                        clockTargetManager._compass = Object.FindObjectOfType<Compass>();
+                        clockTargetManager.playerInput = Object.FindObjectOfType<PlayerInput>();
+                        target.SetActive(false);
+                    }
+                }
+
+                if (larger_clock.Value) { 
+                    azimuthHUD.GetComponent<RectTransform>().anchoredPosition = new Vector2(154.6f, 0f);
+                    azimuthHUD.GetComponent<RectTransform>().localScale = new Vector3(1.4f, 1.4f, 0f);
+                }
             }
 
             if (gameManager.transform.Find("UIHUDCanvas/weapons text").gameObject.GetComponent<AzimuthText>() == null) {
                 if (mils_readout.Value)
                 {
                     AzimuthText azimuthText = gameManager.transform.Find("UIHUDCanvas/weapons text").gameObject.AddComponent<AzimuthText>();
-                    azimuthText._weaponText = gameManager.transform.Find("UIHUDCanvas/weapons text").gameObject.GetComponent<Text>();
-                    azimuthText._weaponHUD = gameManager.transform.Find("UIHUDCanvas/weapons text").gameObject.GetComponent<WeaponHud>();
-                    azimuthText.playerInput = GameObject.FindObjectOfType<PlayerInput>();
+                    azimuthText._weaponText = gameManager.transform.Find("UIHUDCanvas/weapons text").GetComponent<Text>();
+                    azimuthText._weaponHUD = gameManager.transform.Find("UIHUDCanvas/weapons text").GetComponent<WeaponHud>();
+                    azimuthText.playerInput = Object.FindObjectOfType<PlayerInput>();
                 }
             }
             
